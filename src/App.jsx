95732646aae2ce40790257f9d43c46b91c46b7f9
Rheno001 <renfredreuben@gmail.com>
2024-12-15 07:29:21 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return(<div className='px-[20px] bg-black'>
     <Navbar/>
     <section className='p-[50px] px-[150px]'>
-     <div className='hero flex flex-col items-center bg-cover bg-no-repeat bg-center py-[60px] lg:py-[100px] lg:mt-[50px]' style={{ backgroundImage: `url(${gridlines})` }}>
+     <div className='hero flex flex-col items-center bg-cover bg-no-repeat bg-center py-[60px] lg:py-[100px] mt-[50px]' style={{ backgroundImage: `url(${gridlines})` }}>
       <div className='flex flex-col items-center lg:items-end justify-center mt-[-50px] lg:mt-[-100px]'>
         <h1 className='uppercase text-[60px] lg:text-[150px] gap-[25px] font-extrabold text-[#E26E23]'>Marketing</h1>
         <div className='flex items-center justify-end lg:ml-[200px] mt-[-20px] lg:mt-[-80px]'>
@@ -19,9 +19,9 @@ function App() {
         <p className='text-white text-[12px] text-center lg:text-[20px] lg:mt-[-20px]'>Creative Mind, Creative Works</p>
         <span></span>
       </div>
-      <div className='flex items-center justify-center mt-[30px] lg:mt-[80px]'>
-        <p className='text-[6px] lg:text-[15px] text-[white]'>Modern <span className='text-[#E26E23]'>Problem </span>Requires Modern <span className='text-[#E26E23]'>Solution</span></p>
-        <button className='mx-[10px] text-[5px] lg:text-[12px] bg-[#E26E23] flex items-center justify-center text-white rounded-xl h-[15px] lg:h-[30px] p-[12px] lg:p-[18px]'>Lets Connect <img src={vector} alt="" className='h-[8px] lg:h-[20px] mx-[5px] lg:mx-[10px]' /></button>
+      <div className='flex flex-col md:flex-row items-center justify-center mt-[30px] lg:mt-[80px]'>
+        <p className='lg:text-[15px] text-center  text-[white]'>Modern <span className='text-[#E26E23]'>Problem </span>Requires Modern <span className='text-[#E26E23]'>Solution</span></p>
+        <button className='mx-[10px] text-[5px] lg:text-[12px] bg-[#E26E23] flex items-center justify-center text-white rounded-xl h-[15px] lg:h-[30px] p-[12px] lg:p-[18px]'>Lets Connect <img src={vector} alt="" className='hidden lg:block h-[8px] lg:h-[20px] mx-[5px] lg:mx-[10px]' /></button>
       </div>
      </div>
 
